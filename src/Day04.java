7f92bfd77a0775c1b21150b111e8ef1a70e6457b
Juan Parera <1420988+jparera@users.noreply.github.com>
2025-12-04 10:20:16 +0100

    refactor: optimize position removal logic

--- src/Day04.java
+++ src/Day04.java
@@ -3,19 +3,13 @@ import util.Terminal;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day04 {
     private static final char ROLL = '@';
-
-    private static final int[][] OFFSETS = {
-            {-1, -1}, {-1, 0}, {-1, 1},
-            {0, -1}, {0, 1},
-            {1, -1}, {1, 0}, {1, 1}
-    };
 
     public static void main() throws IOException {
         var terminal = Terminal.get();
@@ -32,29 +26,31 @@ public class Day04 {
             }
         }
 
-        var toRemove = rollsToRemove(positions);
+        // Find all removable positions
+        var toRemove = positions.parallelStream()
+                .filter(p -> removable(p, positions))
+                .collect(Collectors.toSet());
         var part1 = toRemove.size();
         var part2 = 0;
         while (!toRemove.isEmpty()) {
             positions.removeAll(toRemove);
             part2 += toRemove.size();
-            toRemove = rollsToRemove(positions);
+            // Check neighbours of removed positions for further removals
+            toRemove = toRemove.parallelStream()
+                    .flatMap(Position::neighbours)
+                    .filter(p -> removable(p, positions))
+                    .collect(Collectors.toSet());
         }
 
         terminal.println(part1);
         terminal.println(part2);
     }
 
-    private static Collection<Position> rollsToRemove(Set<Position> positions) {
-        return positions.parallelStream()
-                .filter(p -> removable(p, positions))
-                .toList();
-    }
-
     private static boolean removable(Position p, Set<Position> positions) {
+        if(!positions.contains(p)) return false;
         int count = 0;
-        for (var offset : OFFSETS) {
-            var neighbor = p.offset(offset);
+        for (int[] offset : OFFSETS) {
+            var neighbor = p.add(offset);
             if (positions.contains(neighbor)) {
                 count++;
             }
@@ -62,8 +58,18 @@ public class Day04 {
         return count < 4;
     }
 
+    private static final int[][] OFFSETS = {
+            {-1, -1}, {-1, 0}, {-1, 1},
+            {0, -1}, {0, 1},
+            {1, -1}, {1, 0}, {1, 1}
+    };
+
     record Position(int row, int col) {
-        public Position offset(int[] offset) {
+        public Stream<Position> neighbours() {
+            return Arrays.stream(OFFSETS).map(this::add);
+        }
+
+        public Position add(int[] offset) {
             return new Position(row + offset[0], col + offset[1]);
         }
     }
